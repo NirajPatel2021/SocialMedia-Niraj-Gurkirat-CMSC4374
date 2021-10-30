d315286bb1fffbe4a919f53e9541cab93e14e2e1
GGILL <ggill@cosmodigital.com>
2021-10-30 11:50:50 -0500

    service and proxy config

---
+++ webui/src/app/service/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpContextToken} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  public getUser = (id:number): Observable<{id:number, username:string}> => {
+    return this.http.get<{id:number, username:string}>(`api/user/${id}`);
+  }
+
+  public createUser = (data:any): Observable<{id:number, username:string, password:string}> => {
+    return this.http.post<{id:number, username:string, password:string}>(`api/user/createUser`,data);
+  }
+
+  public updateUser = (data:any): Observable<{id:number, username:string, password:string}> => {
+    return this.http.put<{id:number, username:string, password:string}>(`api/user/updateUser`,data);
+  }
+
+}
