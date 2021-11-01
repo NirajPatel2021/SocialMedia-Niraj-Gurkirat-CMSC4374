e31194883628ce040fb2879fdb330bef23721319
Niraj Patel <90847730+NirajPatel2021@users.noreply.github.com>
2021-10-31 19:25:23 -0500

    friends list works

--- src/main/java/edu/ben/socialmedia/service/UserService.java
+++ src/main/java/edu/ben/socialmedia/service/UserService.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +14,18 @@ import java.util.Map;
 public class UserService {
 
     int currentId = 11;
+
+    static ArrayList<Integer> friends1 = new ArrayList<>(
+            Arrays.asList(2, 3, 4, 5));
+    static ArrayList<Integer> friends2 = new ArrayList<>(
+            Arrays.asList(3, 4, 5, 6));
+    static ArrayList<Integer> friends3 = new ArrayList<>(
+            Arrays.asList(4, 5, 6, 7));
+    static ArrayList<Integer> friends4 = new ArrayList<>(
+            Arrays.asList(5, 6, 7, 8));
+    static ArrayList<Integer> friends5 = new ArrayList<>(
+            Arrays.asList(6, 7, 8, 9));
+
 
     public static Map<Integer, UserDTO> getAllUsers() {
         return realUserMap;
@@ -38,14 +52,14 @@ public class UserService {
     // Loops through hashmap to see if username and password match
     // if so return id
     // if not return null
-    public UserDTO checkCredentials(UserDTO user1){
+    public UserDTO checkCredentials(UserDTO user1) {
         UserDTO userdto;
         System.out.println(realUserMap.size());
-        for (int i = 1; i <= realUserMap.size(); i++){
+        for (int i = 1; i <= realUserMap.size(); i++) {
             System.out.println(i + "IIIIII");
             System.out.println(user1.toString() + "user 1");
-            if (realUserMap.get(i).getUsername().equals(user1.getUsername())){
-                if (realUserMap.get(i).getPassword().equals(user1.getPassword())){
+            if (realUserMap.get(i).getUsername().equals(user1.getUsername())) {
+                if (realUserMap.get(i).getPassword().equals(user1.getPassword())) {
                     System.out.println("YES");
 
                     return realUserMap.get(i);
@@ -64,16 +78,20 @@ public class UserService {
 //    }
 
     private static Map<Integer, UserDTO> userMap = Map.of(
-            1, UserDTO.of(1, "luffy", "captain"),
-            2, UserDTO.of(2, "zoro", "1stmate"),
-            3, UserDTO.of(3, "nami", "navigator"),
-            4, UserDTO.of(4, "usopp", "blacksmith"),
-            5, UserDTO.of(5, "sanji", "cook"),
+
+            1, UserDTO.of(1, "luffy", "captain", friends1, friends1),
+            2, UserDTO.of(2, "zoro", "1stmate", friends2, friends2),
+            3, UserDTO.of(3, "nami", "navigator", friends3, friends3),
+            4, UserDTO.of(4, "usopp", "blacksmith", friends4, friends4),
+            5, UserDTO.of(5, "sanji", "cook", friends5, friends5),
             6, UserDTO.of(6, "chopper", "doctor"),
             7, UserDTO.of(7, "robin", "scholor"),
             8, UserDTO.of(8, "franky", "shipwright"),
             9, UserDTO.of(9, "brook", "musician"),
-            10, UserDTO.of(10, "jimbei", "helmsman"));
+            10, UserDTO.of(10, "jimbei", "helmsman")
+
+    );
+
 
     private static Map<Integer, UserDTO> realUserMap = new HashMap<>(userMap);
 }
--- webui/src/app/friends/friends.component.ts
+++ webui/src/app/friends/friends.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../service/user.service";
+import {HttpClient} from "@angular/common/http";
+
+class users {
+  constructor(
+    public id: number,
+    public username: string,
+    public password: string,
+    public friends: [],
+    public feed: []
+  ) {
+  }
+}
 
 @Component({
   selector: 'app-friends',
@@ -6,10 +19,91 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends.component.scss']
 })
 export class FriendsComponent implements OnInit {
+  title = 'Friends';
 
-  constructor() { }
+  users: users[] = [];
+  friends: users[] = [];
+
+  LoggedUserName: string | null = null
+  LoggedId: number = 1;
+
+  public user: { id: number, username: String, password: String, friends: [], feed: [] } | null = null
+  userId: number = 1;
+  public errorMessage: string = '';
+
+
+  constructor(
+    private userService: UserService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.LoggedUserName = sessionStorage.getItem("username")
+    this.findLoggedId()
+    this.getUsers();
   }
+
+  getUsers() {
+
+    this.userService.getAllUsers().subscribe(response => {
+
+      let j = 0;
+      while (j in response[this.LoggedId].friends) {
+        console.log(response[this.LoggedId].friends[j])
+
+        let newuser = <users>({
+          id: response[response[this.LoggedId].friends[j]].id,
+          username: response[response[this.LoggedId].friends[j]].username,
+          password: response[response[this.LoggedId].friends[j]].password,
+          friends: response[response[this.LoggedId].friends[j]].friends,
+          feed: response[response[this.LoggedId].friends[j]].feed
+        })
+        this.friends.push(newuser);
+
+
+        j++
+      }
+
+
+      this.users = [];
+
+      let i = 1;
+
+      while (i in response) {
+
+        let newuser = <users>({
+          id: response[i].id,
+          username: response[i].username,
+          password: response[i].password,
+          friends: response[i].friends,
+          feed: response[i].feed
+        })
+        this.users.push(newuser);
+
+        i++;
+      }
+
+
+    });
+  }
+
+  findLoggedId() {
+
+    this.userService.getAllUsers().subscribe(response => {
+
+      let i = 1;
+      while (i in response) {
+        if (response[i].username === this.LoggedUserName)
+        {
+          this.LoggedId = response[i].id
+          break;
+        }
+
+        i++;
+      }
+
+    });
+  }
+
 
 }
