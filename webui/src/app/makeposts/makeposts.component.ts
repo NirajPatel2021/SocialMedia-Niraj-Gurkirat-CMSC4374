549f96bf042d554a4a08609c36192fe05efba98c
Niraj Patel <90847730+NirajPatel2021@users.noreply.github.com>
2021-11-01 16:17:12 -0500

    create post fully functional now
    just changed it so postedBy is now set to current logged-in-user's ID instead of "1"

--- webui/src/app/makeposts/makeposts.component.ts
+++ webui/src/app/makeposts/makeposts.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { PostService } from "../service/post.service";
+import {Component, OnInit} from '@angular/core';
+import {PostService} from "../service/post.service";
+import {UserService} from "../service/user.service";
 
 @Component({
   selector: 'app-makeposts',
@@ -10,31 +11,50 @@ export class MakepostsComponent implements OnInit {
   title = "Posts"
   name = sessionStorage.getItem('username')
 
-
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private userService: UserService) {
+  }
 
   ngOnInit(): void {
-  }
-
-  temptext:string = "post text";
-  temptime:string = new Date().toString();
-
-  public createPost = (data:any) => {
-    this.postService.createPost(data).subscribe((resp)=>{
-       alert(JSON.stringify(resp));
-    }, err=> {
-      alert(JSON.stringify(err));
-    })
+    this.findLoggedId()
 
   }
 
-  public updatePost = (data:any) => {
-    this.postService.updatePost(data).subscribe((resp)=>{
+  temptext: string = "post text";
+  temptime: string = new Date().toString();
+  LoggedId: number = 0;
+
+  public createPost = (data: any) => {
+    this.postService.createPost(data).subscribe((resp) => {
       alert(JSON.stringify(resp));
-    }, err=> {
+    }, err => {
       alert(JSON.stringify(err));
     })
 
+  }
+
+  public updatePost = (data: any) => {
+    this.postService.updatePost(data).subscribe((resp) => {
+      alert(JSON.stringify(resp));
+    }, err => {
+      alert(JSON.stringify(err));
+    })
+
+  }
+
+
+  findLoggedId() {
+    this.userService.getAllUsers().subscribe(response => {
+      let CurrentUsername = sessionStorage.getItem('username')
+
+      let i = 1;
+      while (i in response) {
+        if (response[i].username === CurrentUsername) {
+          this.LoggedId = response[i].id
+          break;
+        }
+        i++;
+      }
+    });
   }
 
 }
